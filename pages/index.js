e7523f5381d0443debb8856c13ff09e0c79caa9f
Jay Mehta <jake2001007@gmail.com>
2021-04-27 22:45:12 +0530

    Some Design Optimizations

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-
+import Image from "next/image";
 const Home = () => {
   return (
     <>
@@ -13,7 +13,12 @@ const Home = () => {
             <div className="profile">
               <div className="profile-picture">
                 <a href="https://codegra.in">
-                  <img className="head-logo"></img>
+                  <Image
+                    src="/images/logo.jpg"
+                    width="150"
+                    height="150"
+                    className="head-logo"
+                  ></Image>
                 </a>
               </div>
               <div className="profile-details">
@@ -36,42 +41,42 @@ const Home = () => {
               <div className="link">
                 <a href="https://codegra.in/blog/blog-6" target="_blank">
                   <div className="right">
-                    <h3>4 less known HTML tags</h3>
-                    <p>Lorem ipsum dolor, sit?</p>
+                    <h2>4 less known HTML tags</h2>
+                    <p>Pretty useful stuff!</p>
                   </div>
                 </a>
               </div>
               <div className="link">
                 <a href="https://codegra.in/blog/blog-5" target="_blank">
                   <div className="right">
-                    <h3>10 key takeaways from show your work</h3>
-                    <p>Lorem ipsum dolor, sit?</p>
+                    <h2>10 key takeaways from show your work</h2>
+                    <p>Austin Kleon is 🔥</p>
                   </div>
                 </a>
               </div>
               <div className="link">
                 <a href="https://codegra.in/blog/blog-4" target="_blank">
                   <div className="right">
-                    <h3>Open Source Apps for Windows, We use at Codegrain</h3>
-                    <p>Lorem ipsum dolor, sit?</p>
+                    <h2>Open Source Apps for Windows, We use at Codegrain</h2>
+                    <p>We use at Codegrain</p>
                   </div>
                 </a>
               </div>
               <div className="link">
                 <a href="https://codegra.in/blog/blog-3" target="_blank">
                   <div className="right">
-                    <h3>Open Source Apps for Mac OS, We use at Codegrain</h3>
-                    <p>Lorem ipsum dolor, sit?</p>
+                    <h2>Open Source Apps for Mac OS, We use at Codegrain</h2>
+                    <p>We use at Codegrain</p>
                   </div>
                 </a>
               </div>
               <div className="link">
                 <a href="https://codegra.in/blog/blog-2" target="_blank">
                   <div className="right">
-                    <h3>
+                    <h2>
                       Master frost gradient, Create frost gradients with ease.
-                    </h3>
-                    <p>Lorem ipsum dolor, sit?</p>
+                    </h2>
+                    <p>Create frost gradients with ease.</p>
                   </div>
                 </a>
               </div>
@@ -79,18 +84,21 @@ const Home = () => {
             <h2>Profile &amp; Social</h2>
             <div className="links-container grids">
               <div className="social-link">
-                <a href="https://www.instagram.com/codegra.in/" target="_blank">
+                <a
+                  href="https://www.instagram.com/distracteddesigner/"
+                  target="_blank"
+                >
                   <div className="bottom">
                     <h3>Instagram</h3>
-                    <p>@codegra.in</p>
+                    <p>@distracteddesigner</p>
                   </div>
                 </a>
               </div>
               <div className="social-link">
-                <a href="https://www.behance.net/CodeGrain" target="_blank">
+                <a href="https://t.me/Elliot_69" target="_blank">
                   <div className="bottom">
-                    <h3>Behance</h3>
-                    <p>@CodeGrain</p>
+                    <h3>Telegram</h3>
+                    <p>@Elliot_69</p>
                   </div>
                 </a>
               </div>
@@ -106,10 +114,10 @@ const Home = () => {
                 </a>
               </div>
               <div className="social-link">
-                <a href="https://twitter.com/codegrain" target="_blank">
+                <a href="https://twitter.com/mjay2001007" target="_blank">
                   <div className="bottom">
                     <h3>Twitter</h3>
-                    <p>@Codegrain</p>
+                    <p>@mjay2001007</p>
                   </div>
                 </a>
               </div>
@@ -118,7 +126,7 @@ const Home = () => {
               <a href="mailto:codegrain@gmail.com" target="_blank">
                 <div className="bottom">
                   <h3>Let&rsquo;s connect via Mail</h3>
-                  <p>codegrain@gmail.com</p>
+                  <p>jake2001007@gmail.com</p>
                 </div>
               </a>
             </div>
